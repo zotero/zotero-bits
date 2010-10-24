de1a5144cba52a83f871045e3f5acb905144ce31
Avram Lyon <ajlyon@gmail.com>
2010-10-24 20:09:25 +0400

    Adding ISBN checking logic code

---
+++ isbn.js
@@ -0,0 +1,79 @@
+// Implementation of ISBN check-digit verification
+// Based on ISBN Users' Manual (http://www.isbn.org/standards/home/isbn/international/html/usm4.htm)
+// and the Wikipedia treatment of ISBN (http://en.wikipedia.org/wiki/International_Standard_Book_Number)
+
+// This does not validate multiple ISBNs in one field,
+// but it will gracefully ignore all non-number detritus,
+// such as hyphens, spaces, and comments.
+
+// Takes the first 10 valid digits and tries to read an ISBN 10,
+// and takes the first 13 valid digits to try to read an ISBN 13
+// Returns an four-element array:
+// 	[0] the validity as an ISBN 10
+// 	[1] detected ISBN 10 that checks
+// 	[2] validity as an ISBN 13
+// 	[3] detected ISBN 13 that checks
+// If there is no valid ISBN of a type detected, then elements 1 and/or 3 will
+// be empty. There could conceivably be a valid ISBN-13 with an ISBN-10
+// substring; this should probably be interpreted as the latter, but it is a
+// client UI issue.
+ISBNCheck = function(isbn) {
+	// For ISBN 10, multiple by these coefficients, take the sum mod 11
+	// and subtract from 11
+	var isbn10 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+	// For ISBN 13, multiple by these coefficients, take the sum mod 10
+	// and subtract from 10
+	var isbn13 = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
+
+	// We make a single pass through the provided string, interpreting the
+	// first 10 valid characters as an ISBN-10, and the first 13 as an
+	// ISBN-13. We then return an array of booleans and valid detected
+	// ISBNs.
+
+	var j = 0;
+	var sum10 = 0;
+	var num10 = "";
+	var sum13 = 0;
+	var num13 = "";
+	var chars = [];
+
+	for (var i=0; i < isbn.length; i++) {
+		if (isbn.charAt(i) == " ") {
+			// Since the space character evaluates as a number,
+			// it is a special case.
+		} else if (j < 9 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			sum10 += isbn.charAt(i) * isbn10[j];
+			sum13 += isbn.charAt(i) * isbn13[j];
+			num10 += isbn.charAt(i);
+			num13 += isbn.charAt(i);
+			j++;
+		} else if (j == 9 &&
+			(isbn.charAt(i) == "X" || isbn.charAt(i) == "x" ||
+				((isbn.charAt(i) - 0) == isbn.charAt(i)))) {
+			// In ISBN-10, an X represents the check digit "10".
+			if(isbn.charAt(i) == "X" || isbn.charAt(i) == "x") {
+				var check10 = 10;
+				num10 += "X";
+			} else {
+				var check10 = isbn.charAt(i);
+				sum13 += isbn.charAt(i) * isbn13[j];
+				num10 += isbn.charAt(i);
+				num13 += isbn.charAt(i);
+				j++;
+			}
+		} else if(j < 12 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			sum13 += isbn.charAt(i) * isbn13[j];
+			num13 += isbn.charAt(i);
+			j++;
+		} else if (j == 12 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			var check13 = isbn.charAt(i);
+			num13 += isbn.charAt(i);
+		}
+	}
+	var valid10 = (11 - (sum10 % 11) == check10);
+	var valid13 = (10 - (sum13 % 10) == check13);
+	if(!valid10) {num10 = ""};
+	if(!valid13) {num13 = ""};
+	return [valid10, num10, valid13, num13];
+}
