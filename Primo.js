710a362bc7409021cd8191d93c37d1bd67b94eac
Avram Lyon <ajlyon@gmail.com>
2010-11-29 11:38:59 +0300

    Migrating back to PNX, now with ISO 639-2 lookup and ISBN validation!

--- Primo.js
+++ Primo.js
@@ -26,6 +26,7 @@ function detectWeb(doc, url) {
 		}
 }
 
+// There is code for handling RIS, but let's stick with PNX for now.
 
 function doWeb(doc, url) {
 	var namespace = doc.documentElement.namespaceURI;
@@ -47,11 +48,17 @@ function doWeb(doc, url) {
 		// try/catch for the case when there are no search results, let doc.evaluate fail quietly
 		try {
 			while (link = linkIterator.iterateNext(), title = titleIterator.iterateNext()) {
+				// XXX RIS BLOCK
 				// create an array containing the links to RIS
-			 	var link_pieces = RISregex.exec(link.textContent);
+			 	/*var link_pieces = RISregex.exec(link.textContent);
 				var title = Zotero.Utilities.trimInternal(title.textContent);
 				var linkRIS = link_pieces[1]+"?ct=display&"+link_pieces[2]+"&showRIS=true&afterPDS=true";
 				items[linkRIS] = title;
+				*/
+				// create an array containing the links and add '&showPnx=true' to the end
+			 	var xmlLink = Zotero.Utilities.trimInternal(link.textContent)+'&showPnx=true';
+				var title = Zotero.Utilities.trimInternal(title.textContent);
+				items[xmlLink] = title;
 			}
 			items = Zotero.selectItems(items);
 			if(!items) return;
@@ -65,11 +72,15 @@ function doWeb(doc, url) {
 
 		
 	} else {
-		var link_pieces = RISregex.exec(url);
+		links.push(url+'&showPnx=true');
+		// XXX RIS BLOCK
+		/*var link_pieces = RISregex.exec(url);
 		var linkRIS = link_pieces[1]+"?ct=display&"+link_pieces[2]+"&showRIS=true&afterPDS=true";
-		links.push(linkRIS);
+		links.push(linkRIS);*/
 	}
 	
+	// XXX RIS BLOCK
+	/*
 	Zotero.Utilities.processDocuments(links, function(doc) {
 		var text = doc.evaluate('//textarea[@name="ImportData"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
 		text = text.replace(/[ \t]*$/gm,"");
@@ -85,6 +96,218 @@ function doWeb(doc, url) {
 		translator.translate();
 	}, function() {Zotero.done();});
 	Zotero.wait();
+	*/
+	
+	Zotero.Utilities.HTTP.doGet(links, pnxToItem, function() {Zotero.done();});
+	Zotero.wait();
+}
+
+function pnxToItem(text) {
+		text = text.replace(/^<\?xml\s+version\s*=\s*(["'])[^\1]+\1[^?]*\?>/, ""); //because E4X is full of FAIL
+		var xmldoc = new XML(text);
+		
+		if (xmldoc.display.type.toString() == 'book') {
+			var item = new Zotero.Item("book");
+		} else if (xmldoc.display.type.toString() == 'audio') {
+			var item = new Zotero.Item("audioRecording");
+		} else if (xmldoc.display.type.toString() == 'video') {
+			var item = new Zotero.Item("videoRecording");
+		} else {
+			var item = new Zotero.Item("document");
+		}
+		item.title = xmldoc.display.title.toString();
+		
+		var creators = xmldoc.display.creator.toString().replace(/\d{4}-(\d{4})?/, '').split("; ");
+		var contributors = xmldoc.display.contributor.toString().replace(/\d{4}-(\d{4})?/, '').split("; ");
+		
+		if (!creators[0]) { // <contributor> not available using <contributor> as author instead
+			creators = contributors;
+			contributors = null;
+		}
+		for (creator in creators) {
+			if (creators[creator]) {
+				item.creators.push(Zotero.Utilities.cleanAuthor(creators[creator], "author"));
+			}
+		}
+		
+		for (contributor in contributors) {
+			if (contributors[contributor]) {
+				item.creators.push(Zotero.Utilities.cleanAuthor(contributors[contributor], "contributor"));
+			}
+		}
+		
+		var pubplace = xmldoc.display.publisher.toString().split(" : ");
+		if (pubplace) {
+			item.place = pubplace[0];
+			item.publisher = pubplace[1];
+		}
+		
+		var date = xmldoc.display.creationdate.toString();
+		if (date) item.date = date.match(/\d+/)[0];
+		
+		var language = xmldoc.display.language.toString();
+		// We really hope that Primo always uses ISO 639-2
+		if (language) item.language = iso6392(language);
+		
+		var pages = xmldoc.display.format.toString().match(/(\d+)\sp\./);
+		if (pages) item.pages = pages[1];
+	
+		// The identifier field is supposed to have standardized format, but
+		// the super-tolerant idCheck should be better than a regex.
+		// (although note that it will reject invalid ISBNs)	
+		var locators = idCheck(xmldoc.display.identifier.toString());
+		if (locators.isbn10) item.ISBN = locators.isbn10;
+		if (locators.isbn13) item.ISBN = locators.isbn13;
+		if (locators.issn) item.ISSN = locators.issn;
+		
+		var edition = xmldoc.display.edition.toString();
+		if (edition) item.edition = edition;
+		
+		for each (subject in xmldoc.search.subject) {
+			item.tags.push(subject.toString());
+		}
+		// does callNumber get stored anywhere else in the xml?
+		item.callNumber = xmldoc.enrichment.classificationlcc[0];
+		item.complete();		
+}
+
+/* The next two functions are logic that could be bundled away into the translator toolkit. */
+
+// Implementation of ISBN and ISSN check-digit verification
+// Based on ISBN Users' Manual (http://www.isbn.org/standards/home/isbn/international/html/usm4.htm)
+// and the Wikipedia treatment of ISBN (http://en.wikipedia.org/wiki/International_Standard_Book_Number)
+// and the Wikipedia treatment of ISSN (http://en.wikipedia.org/wiki/International_Standard_Serial_Number)
+
+// This will also check ISMN validity, although it does not distinguish from their
+// neighbors in namespace, ISBN-13. It does not handle pre-2008 M-prefixed ISMNs; see
+// http://en.wikipedia.org/wiki/International_Standard_Music_Number
+
+// This does not validate multiple identifiers in one field,
+// but it will gracefully ignore all non-number detritus,
+// such as extraneous hyphens, spaces, and comments.
+
+// It currently maintains hyphens in non-initial and non-final position,
+// discarding consecutive ones beyond the first as well.
+
+// It also adds the customary hyphen to valid ISSNs.
+
+// Takes the first 8 valid digits and tries to read an ISSN,
+// takes the first 10 valid digits and tries to read an ISBN 10,
+// and takes the first 13 valid digits to try to read an ISBN 13
+// Returns an object with three attributes:
+// 	"issn" 
+// 	"isbn10"
+// 	"isbn13"
+// Each will be set to a valid identifier if found, and otherwise be a
+// boolean false.
+
+// There could conceivably be a valid ISBN-13 with an ISBN-10
+// substring; this should probably be interpreted as the latter, but it is a
+// client UI issue.
+idCheck = function(isbn) {
+	// For ISBN 10, multiple by these coefficients, take the sum mod 11
+	// and subtract from 11
+	var isbn10 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+	// For ISBN 13, multiple by these coefficients, take the sum mod 10
+	// and subtract from 10
+	var isbn13 = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
+
+	// For ISSN, multiply by these coefficients, take the sum mod 11
+	// and subtract from 11
+	var issn = [8, 7, 6, 5, 4, 3, 2];
+
+	// We make a single pass through the provided string, interpreting the
+	// first 10 valid characters as an ISBN-10, and the first 13 as an
+	// ISBN-13. We then return an array of booleans and valid detected
+	// ISBNs.
+
+	var j = 0;
+	var sum8 = 0;
+	var num8 = "";
+	var sum10 = 0;
+	var num10 = "";
+	var sum13 = 0;
+	var num13 = "";
+	var chars = [];
+
+	for (var i=0; i < isbn.length; i++) {
+		if (isbn.charAt(i) == " ") {
+			// Since the space character evaluates as a number,
+			// it is a special case.
+		} else if (j > 0 && isbn.charAt(i) == "-" && isbn.charAt(i-1) != "-") {
+			// Preserve hyphens, except in initial and final position
+			// Also discard consecutive hyphens
+			if(j < 7) num8 += "-";
+			if(j < 10) num10 += "-";
+			if(j < 13) num13 += "-";
+		} else if (j < 7 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			sum8 += isbn.charAt(i) * issn[j];
+			sum10 += isbn.charAt(i) * isbn10[j];
+			sum13 += isbn.charAt(i) * isbn13[j];
+			num8 += isbn.charAt(i);
+			num10 += isbn.charAt(i);
+			num13 += isbn.charAt(i);
+			j++;
+		} else if (j == 7 &&
+			(isbn.charAt(i) == "X" || isbn.charAt(i) == "x" ||
+				((isbn.charAt(i) - 0) == isbn.charAt(i)))) {
+			// In ISSN, an X represents the check digit "10".
+			if(isbn.charAt(i) == "X" || isbn.charAt(i) == "x") {
+				var check8 = 10;
+				num8 += "X";
+			} else {
+				var check8 = isbn.charAt(i);
+				sum10 += isbn.charAt(i) * isbn10[j];
+				sum13 += isbn.charAt(i) * isbn13[j];
+				num8 += isbn.charAt(i);
+				num10 += isbn.charAt(i);
+				num13 += isbn.charAt(i);
+				j++;
+			}
+		} else if (j < 9 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			sum10 += isbn.charAt(i) * isbn10[j];
+			sum13 += isbn.charAt(i) * isbn13[j];
+			num10 += isbn.charAt(i);
+			num13 += isbn.charAt(i);
+			j++;
+		} else if (j == 9 &&
+			(isbn.charAt(i) == "X" || isbn.charAt(i) == "x" ||
+				((isbn.charAt(i) - 0) == isbn.charAt(i)))) {
+			// In ISBN-10, an X represents the check digit "10".
+			if(isbn.charAt(i) == "X" || isbn.charAt(i) == "x") {
+				var check10 = 10;
+				num10 += "X";
+			} else {
+				var check10 = isbn.charAt(i);
+				sum13 += isbn.charAt(i) * isbn13[j];
+				num10 += isbn.charAt(i);
+				num13 += isbn.charAt(i);
+				j++;
+			}
+		} else if(j < 12 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			sum13 += isbn.charAt(i) * isbn13[j];
+			num13 += isbn.charAt(i);
+			j++;
+		} else if (j == 12 && ((isbn.charAt(i) - 0) == isbn.charAt(i))) {
+			var check13 = isbn.charAt(i);
+			num13 += isbn.charAt(i);
+		}
+	}
+	var valid8  = ((11 - sum8 % 11) % 11) == check8;
+	var valid10 = ((11 - sum10 % 11) % 11) == check10;
+	var valid13 = (10 - sum13 % 10 == check13);
+	var matches = false;
+	
+	// Since ISSNs have a standard hyphen placement, we can add a hyphen
+	if (valid8 && (matches = num8.match(/([0-9]{4})([0-9]{3}[0-9Xx])/))) {
+		num8 = matches[1] + '-' + matches[2];
+	} 
+
+	if(!valid8) {num8 = false};
+	if(!valid10) {num10 = false};
+	if(!valid13) {num13 = false};
+	return {"isbn10" : num10, "isbn13" : num13, "issn" : num8};
 }
 
 // Gives name for three-letter code
