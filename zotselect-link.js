4bab83b5cb3241d9b85eb71e8fb45b0a2ad42f0e
Avram Lyon <ajlyon@gmail.com>
2011-01-07 01:23:41 +0300

    re-adding zotselect

---
+++ zotselect-link.js
@@ -0,0 +1,25 @@
+{
+"translatorID":"04623cf0-313c-11df-9aae-0800200c9a66",
+"translatorType":2,
+"label":"ZotSelect Link",
+"creator":"Scott Campbell, Avram Lyon",
+"target":"html",
+"minVersion":"2.0",
+"maxVersion":"",
+"priority":200,
+"inRepository":false,
+"lastUpdated":"2010-03-12 10:51:00"
+}
+
+Zotero.configure("dataMode", "block");
+Zotero.addOption("exportCharset", "UTF-8");
+
+function doExport() {
+	var item;
+	while(item = Zotero.nextItem()) {
+		Zotero.write("<a href='zotero://select//");
+		var library_id = item.LibraryID ? item.LibraryID : 0;
+		Zotero.write(library_id+"_"+item.key+"'>");
+		Zotero.write(item.creators[0].lastName+". "+item.date+". <i>"+item["title"]+"</i>"+"</a>");
+	}
+}
