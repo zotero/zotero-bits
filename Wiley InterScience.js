c9b1a0c21f5a84e41ef1dc8f32faf565bf880090
Avram Lyon <ajlyon@gmail.com>
2011-02-04 23:47:48 +0300

    Tore up and redid Wiley

--- Wiley InterScience.js
+++ Wiley InterScience.js
@@ -1,9 +1,9 @@
 {
 	"translatorID":"fe728bc9-595a-4f03-98fc-766f1d8d0936",
 	"translatorType":4,
-	"label":"Wiley InterScience",
-	"creator":"Sean Takats and Michael Berkowitz",
-	"target":"https?:\\/\\/(?:www3\\.|www\\.)?interscience\\.wiley\\.com[^\\/]*\\/(?:search\\/|(cgi-bin|journal)\\/[0-9]+\\/abstract|journal)",
+	"label":"Wiley Online Library",
+	"creator":"Sean Takats, Michael Berkowitz and Avram Lyon",
+	"target":"^https?://onlinelibrary\\.wiley\\.com[^\\/]*/doi",
 	"minVersion":"1.0.0b4.r5",
 	"maxVersion":"",
 	"priority":100,
@@ -17,17 +17,7 @@ function detectWeb(doc, url){
 		if (prefix == 'x') return namespace; else return null;
 	} : null;
 		
-	var xpath = '//input[@name="ID"][@type="checkbox"]';
-	if(doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-		return "multiple";
-	}
-	if (url.match(/journal\/\d+\/(issue|home)$/)) {
-		return "multiple";
-	}
-	var m = url.match(/https?:\/\/[^\/]*\/(cgi-bin|journal)(\/(abstract|summary))?\/[0-9]+\/abstract/);
-	if (m){
-		return "journalArticle";
-	}
+	return "journalArticle";
 }
 
 function doWeb(doc, url){
@@ -36,32 +26,11 @@ function doWeb(doc, url){
 		if (prefix == 'x') return namespace; else return null;
 	} : null;
 	var host = 'http://' + doc.location.host + "/";
-	Zotero.debug(host);
-	var m = url.match(/https?:\/\/[^\/]*\/(journal|cgi-bin\/summary)\/([0-9]+)\/(abstract)?/);
+	
 	var ids = new Array();
 	if(detectWeb(doc, url) == "multiple") {  //search
-		var id;
 		var title;
 		var availableItems = new Array();
-		var xpath = '//tr[td/input[@name="ID"][@type="checkbox"]]';
-		if (doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-			elmts = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-			var elmt = elmts.iterateNext();
-			do {
-				title = doc.evaluate('./td/strong', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-				id = doc.evaluate('./td/input[@name="ID"][@type="checkbox"]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().value;
-				availableItems[id] = title;
-			} while (elmt = elmts.iterateNext())
-		} else {
-			var xpath = '//div[@id="contentCell"]/div[*/a]';
-			var elmts = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-			var elmt = elmts.iterateNext();
-			do {
-				title = Zotero.Utilities.trimInternal(doc.evaluate('.//strong', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent);
-				id = doc.evaluate('.//a[1]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().href.match(/\/([\d]+)\/abstract/)[1];
-				availableItems[id] = title;
-			} while (elmt = elmts.iterateNext())
-		}
 		var items = Zotero.selectItems(availableItems);
 		if(!items) {
 			return true;
@@ -69,57 +38,102 @@ function doWeb(doc, url){
 		for(var id in items) {
 			ids.push(id);
 		}
-		
-	} else if (m){ //single article
-		ids.push(m[2]);
+		Zotero.Utilities.processDocuments(urls, scrape, function () { Zotero.done(); });
+	} else { //single article
+		scrape(doc, url);
 	}
 	
-	
-	var sets = [];
-	for each (id in ids) {
-		var uri = host + 'tools/citex';
-		var poststring = "clienttype=1&subtype=1&mode=1&version=1&id=" + id;
-		sets.push({ id: id, uri: uri, poststring: poststring });
-	}
-	
-	var setupCallback = function (set, next) {
-		Zotero.Utilities.HTTP.doPost(set.uri, set.poststring, function () {
-			next();
-		});
-	}
-	
-	var processCallback = function (set, next) {
-		var id = set.id;
-		var uri = host+"tools/CitEx";
-		var poststring = "mode=2&format=3&type=2&file=3&exportCitation.x=16&exportCitation.y=10&exportCitation=submit";
-		Zotero.Utilities.HTTP.doPost(uri, poststring, function(text) {
-			var m = text.match(/%A\s(.*)/);  //following lines fix Wiley's incorrect %A tag (should be separate tags for each author)
-			if (m){
-				var newauthors ="";
-				var authors = m[1].split(",")
-				for each (var author in authors){
-					if (author != ""){
-						newauthors = newauthors + "%A "+Zotero.Utilities.unescapeHTML(Zotero.Utilities.trimInternal(author))+"\n";
-					}
-				}
-				text = text.replace(/%A\s.*\n/, newauthors);
-			}
-			var translator = Zotero.loadTranslator("import");
-			translator.setTranslator("881f60f2-0802-411a-9228-ce5f47b64c7d"); //EndNote/Refer/BibIX
-			translator.setString(text);
-			translator.setHandler("itemDone", function(obj, item) {
-				var pdfurl = host + "cgi-bin/fulltext?ID=" + id + "&PLACEBO=IE.pdf&mode=pdf";
-				item.attachments.push({url:pdfurl, title:"Wiley Interscience PDF", mimeType:"application/pdf"});
-				item.DOI = item.url.match(/\.org\/(.*)$/)[1];
-				item.complete();
-			});
-			translator.translate();
-			
-			next();
-		}, null, 'iso-8859-1');
-	}
-	
-	var callbacks = [setupCallback, processCallback];
-	Zotero.Utilities.processAsync(sets, callbacks, function () { Zotero.done(); });
 	Zotero.wait();
+}
+
+function scrape(doc,url)
+{
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? function(prefix) {
+		if (prefix == 'x') return namespace; else return null;
+	} : null;
+       
+	var newItem=new Zotero.Item("journalArticle");
+       var temp;
+       var xpath;
+       var row;
+       var rows;
+
+       newItem.url = doc.location.href;
+       var metaTags = doc.getElementsByTagName("meta");
+
+       var pages = [false, false];
+       var doi = false;
+       var pdf = false;
+       var html = false;
+	for (var i = 0; i< metaTags.length; i++) {
+		var tag = metaTags[i].getAttribute("name");
+		var value = metaTags[i].getAttribute("content");
+		Zotero.debug(pages + pdf + html);
+       		Zotero.debug("Have meta tag: " + tag + " => " + value);
+		switch (tag) {
+			// Dublin Core
+			case "dc.publisher": newItem.publisher = value; break;
+			case "dc.language": newItem.language = value; break;
+			case "dc.rights": newItem.rights = value; break;
+			case "dc.title": newItem.title = value; break;
+			case "dc.creator": newItem.creators.push(Zotero.Utilities.cleanAuthor(value)); break;
+			// This is often NaN for some reason
+			case "dc.date": if (value != "NaN" && value !== "") newItem.date = value; break;
+			case "dc.identifier": if (!newItem.DOI && (doi = value.match(/^doi:(.*)/))) newItem.DOI = doi[1]; break;
+			// PRISM
+			case "prism.publicationName": newItem.publicationTitle = value; break;
+			case "prism.issn": if (!newItem.ISSN && value != "NaN" && value != "") newItem.ISSN = value; break;
+			case "prism.eIssn": if (!newItem.ISSN && value != "NaN" && value != "") newItem.ISSN = value; break;
+			// This is often NaN for some reason
+			case "prism.publicationDate": if (!newItem.date && value != "NaN" && value !== "") newItem.date = value; break;
+			case "prism.volume": if (!newItem.volume && value != "NaN" && value != "") newItem.volume = value; break;
+			case "prism.number": if (!newItem.issue && value != "NaN" && value != "") newItem.issue = value; break;
+			// These also seem bad
+			case "prism.startingPage": if(!pages[0] && value != "null" && value != "") pages[0] = value; break;
+			case "prism.endingPage": if(!pages[1] && value != "null" && value != "") pages[1] = value; break;
+			case "prism.number": newItem.issue = value; break;
+			// Google.
+			case "citation_journal_title": if (!newItem.publicationTitle) newItem.publicationTitle = value; break;
+			case "citation_authors":
+				if (newItem.creators.length == 0) {
+					for each(var author in value.split(';')) newItem.creators.push(Zotero.Utilities.cleanAuthor(author, "author", true));
+				}
+				break;
+			case "citation_title": if (!newItem.title) newItem.title = value; break;
+			case "citation_publisher": if (!newItem.publisher) newItem.publisher = value; break;
+			case "citation_date": if (!newItem.date && value != "NaN" && value != "") newItem.date = value; break;
+			case "citation_year": if (!newItem.date && value != "NaN" && value != "") newItem.date = value; break;
+			case "citation_volume": if (!newItem.volume && value != "NaN" && value != "") newItem.volume = value; break;
+			case "citation_issue": if (!newItem.issue && value != "NaN" && value != "") newItem.issue = value; break;
+			case "citation_firstpage": if (!pages[0] && value != "NaN" && value != "") pages[0] = value; break;
+			case "citation_lastpage": if (!pages[1] && value != "NaN" && value != "") pages[1] = value; break;
+			case "citation_issn": if (!newItem.ISSN && value != "NaN" && value != "") newItem.ISSN = value; break;
+			// Prefer long language names
+			case "citation_language": if ((!newItem.language || newItem.language.length < 4)
+								&& value != "null" && value != "") newItem.language = value; break;
+			case "citation_doi": if (!newItem.DOI) newItem.DOI = value; break;
+			case "citation_abstract": newItem.abstractNote = value; break;
+			case "citation_abstract_html_url": newItem.url = value; break;
+			case "citation_pdf_url": if(!pdf) pdf = value; break;
+			case "citation_keywords": newItem.tags.push(value); break;
+			case "citation_fulltext_html_url": if(!pdf) pdf = value; break;
+			case "fulltext_pdf": if(!pdf) pdf = value; break;
+			default:
+				Zotero.debug("Ignoring meta tag: " + tag + " => " + value);
+		}
+	}
+	
+	if (pdf) newItem.attachments = [{url:pdf, title:"Wiley Full Text PDF", mimeType:"application/pdf"}];
+	if (html) newItem.attachments = [{url:html, title:"Wiley Full Text HTML"}];
+	
+	if (pages[0] && pages[1]) newItem.pages = pages.join('-')
+	else newItem.pages = pages[0] ? pages[1] : (pages[1] ? pages[1] : "");
+
+	// Abstracts don't seem to come with
+	if (!newItem.abstractNote) {
+		var abstractNode = doc.evaluate('//div[@id="abstract"]/div[@class="para"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
+		if (abstractNode) newItem.abstractNote = abstractNode.textContent;
+	}
+       newItem.complete();
 }
