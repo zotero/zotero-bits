196f2fcd76e39556850844d6ba281edc97a18603
Avram Lyon <ajlyon@gmail.com>
2011-03-11 12:16:56 +0300

    Update license date

--- eLibrary.ru.js
+++ eLibrary.ru.js
@@ -13,7 +13,7 @@
 
 /*
    eLibrary.ru Translator
-   Copyright (C) 2010 Avram Lyon, ajlyon@gmail.com
+   Copyright (C) 2010-2011 Avram Lyon, ajlyon@gmail.com
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
