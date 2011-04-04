ed7e90ab57e339048cd36ac92533546352926a79
Avram Lyon <ajlyon@gmail.com>
2011-04-04 23:37:17 +0400

    With one more pattern

--- Library Catalog (Aleph).js
+++ Library Catalog (Aleph).js
@@ -2,7 +2,7 @@
 	"translatorID":"cf87eca8-041d-b954-795a-2d86348999d5",
 	"translatorType":4,
 	"label":"Library Catalog (Aleph)",
-	"creator":"Simon Kornblith and Michael Berkowitz",
+	"creator":"Simon Kornblith, Michael Berkowitz, Ming Yeung Cheung",
 	"target":"https?://[^/]+/F(?:/[A-Z0-9\\-]+(?:\\?.*)?$|\\?func=find|\\?func=scan|\\?func=short)",
 	"minVersion":"1.0.0b3.r1",
 	"maxVersion":"",
@@ -11,9 +11,38 @@
 	"lastUpdated":"2009-10-22 19:00:00"
 }
 
+/*
+Aleph OPAC Translator
+Example installations (mainly French):
+http://naude.bibliotheque-mazarine.fr/
+http://bibli.polytechnique.fr/
+http://sifrix2.sdv.fr/
+http://aleph.insa-rouen.fr
+http://brenet.ens-lyon.fr
+http://bu-pau.univ-pau.fr/
+http://babel.bu.univ-paris5.fr
+http://inti.univ-paris4.fr/
+http://aleph.u-paris10.fr/
+http://servaleph.univ-catholyon.fr/
+http://armada.scd.univ-paris12.fr/
+http://catalogue.univ-angers.fr/
+http://biblio.ville-lehavre.fr/
+http://opac.nebis.ch/
+http://scd2.univ-lille1.fr/
+http://catalogue.univ-paris1.fr/
+http://source.ulg.ac.be/
+http://med.cite-sciences.fr/
+http://biblio.mulhouse.fr/
+http://mediatheque.sigdci76.fr/
+http://opac.biu-montpellier.fr/ 
+
+Multiple save broken:
+http://opac.libis.be/
+*/
+
 function detectWeb(doc, url) {
-	var singleRe = new RegExp("^https?://[^/]+/F/[A-Z0-9\-]+\?.*(?:func=full-set-set.*\&format=[0-9]{3}|func=direct|func=myshelf-full.*)");
-	
+	var singleRe = new RegExp("^https?://[^/]+/F/[A-Z0-9\-]+\?.*(?:func=full-set-set|func=direct|func=myshelf-full|func=myshelf_full.*)");
+
 	if(singleRe.test(doc.location.href)) {
 		return "book";
 	} else {
@@ -27,14 +56,29 @@ function detectWeb(doc, url) {
 }
 
 function doWeb(doc, url) {
-	var detailRe = new RegExp("^https?://[^/]+/F/[A-Z0-9\-]+\?.*(?:func=full-set-set.*\&format=[0-9]{3}|func=direct|func=myshelf-full.*)");
+	var detailRe = new RegExp("^https?://[^/]+/F/[A-Z0-9\-]+\?.*(?:func=full-set-set|func=direct|func=myshelf-full|func=myshelf_full.*)");
 	var mab2Opac = new RegExp("^https?://[^/]+berlin|193\.30\.112\.134|duisburg-essen/F/[A-Z0-9\-]+\?.*");
 	var uri = doc.location.href;
 	var newUris = new Array();
 	
 	if(detailRe.test(uri)) {
-		var newuri = uri.replace(/\&format=[0-9]{3}/, "&format=001");
-		if (newuri == uri) newuri += "&format=001";
+		// find the 'add to basket' link where it will have the document number, replace the function with 'direct'
+		if (doc.evaluate('//*[contains(@href, "myshelf-add-ful-1")]', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+			var elmts_add = doc.evaluate('//*[contains(@href, "myshelf-add-ful-1")]', doc, null, XPathResult.ANY_TYPE, null);
+			var adduri = elmts_add.iterateNext().attributes.getNamedItem("href").value;
+			adduri = adduri.replace("myshelf-add-ful-1", "direct");
+			//adduri = adduri.replace("myshelf-add-ful-1", "myshelf-full");			
+			var baseuri = uri.match(".*\\?");
+			var funcuri = adduri.match("\\?.*");
+			newuri = baseuri + funcuri[0].match("[^\\?].*");
+			newuri += "&format=001";
+			//Zotero.debug('baseuri = ' + baseuri);
+			//Zotero.debug('funcuri = ' + funcuri);
+			Zotero.debug('directuri = ' + newuri);
+		} else {
+			var newuri = uri.replace(/\&format=[0-9]{3}/, "&format=001");
+			if (newuri == uri) newuri += "&format=001";
+		}
 		newUris.push(newuri);
 	} else {
 		var itemRegexp = '^https?://[^/]+/F/[A-Z0-9\-]+\?.*(?:func=full-set-set.*\&format=999|func=direct|func=myshelf-full.*)'
@@ -81,6 +125,7 @@ function doWeb(doc, url) {
 		} : null;
 		var nonstandard = false;
 		var th = false;
+		var div = false;
 		var xpath;
 		if (newDoc.evaluate('//*[tr[td/text()="LDR"]]/tr[td[2]]', newDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
 			xpath = '//*[tr[td/text()="LDR"]]/tr[td[2]]';
@@ -96,14 +141,19 @@ function doWeb(doc, url) {
 		} else if (newDoc.evaluate('//tr/td[2]/table/tbody[tr/td[contains(text(), "LDR")]]', newDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
 			xpath = '//tr/td[2]/table/tbody[tr/td[contains(text(), "LDR")]]/tr';
 			nonstandard = true;
+		} else if (newDoc.evaluate('//*[div[div/text()="LDR"]]/div[@class="fullLine"]', newDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
+		 	xpath = '//*[div[div/text()="LDR"]]/div[@class="fullLine"]';
+			div = true;
 		}
 		var elmts = newDoc.evaluate(xpath, newDoc, nsResolver, XPathResult.ANY_TYPE, null);
 		var elmt;
 		var record = new marc.record();
 		while(elmt = elmts.iterateNext()) {
 			if (th) {
-          var field = Zotero.Utilities.superCleanString(newDoc.evaluate('./th', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent);
-      } else {
+          		var field = Zotero.Utilities.superCleanString(newDoc.evaluate('./th', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent);
+      		} else if (div) {
+				var field = Zotero.Utilities.superCleanString(newDoc.evaluate('./div[1]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent);
+			} else {
           var field = Zotero.Utilities.superCleanString(newDoc.evaluate('./td[1]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent);
       }
       // if (nonstandard) {
@@ -116,6 +166,8 @@ function doWeb(doc, url) {
 				var value;
 				if (th) {
 				    value = newDoc.evaluate('./TD[1]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent; //.split(/\n/)[1];
+				} else if (div) {
+					value = newDoc.evaluate('./div[2]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent; //.split(/\n/)[1];	
 				} else {
 				  value = newDoc.evaluate('./TD[2]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent; //.split(/\n/)[1];
 				}
