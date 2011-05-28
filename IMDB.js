f32f5d972bfe3bb02b050cc08c91745b48683437
Avram Lyon <ajlyon@gmail.com>
2011-05-29 01:54:03 +0400

    add GPL

--- IMDB.js
+++ IMDB.js
@@ -11,6 +11,23 @@
         "lastUpdated": "2011-05-28 14:17:44"
 }
 
+/*
+   IMDB Translator
+   Copyright (C) 2011 Avram Lyon, ajlyon@gmail.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 function detectWeb(doc, url){
 	if (url.match(/\/title\/tt\d+/)) {
