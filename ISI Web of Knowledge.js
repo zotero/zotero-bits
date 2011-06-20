f8f959dd9418e0b8a282a2f7016fcfa62fc9c9e8
Avram Lyon <ajlyon@gmail.com>
2011-06-21 01:26:03 +0400

    Making ISI more capitals-friendly and connector-friendly

--- ISI Web of Knowledge.js
+++ ISI Web of Knowledge.js
@@ -1,14 +1,15 @@
 {
-	"translatorID":"594ebe3c-90a0-4830-83bc-9502825a6810",
-	"translatorType":4,
-	"label":"ISI Web of Knowledge",
-	"creator":"Michael Berkowitz",
-	"target":"(WOS_GeneralSearch|product=WOS|product=CABI)",
-	"minVersion":"2.0",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2010-04-01 22:50:00"
+        "translatorID": "594ebe3c-90a0-4830-83bc-9502825a6810",
+        "label": "ISI Web of Knowledge",
+        "creator": "Michael Berkowitz",
+        "target": "(WOS_GeneralSearch|product=WOS|product=CABI)",
+        "minVersion": "2.0",
+        "maxVersion": "",
+        "priority": 100,
+        "inRepository": true,
+        "translatorType": 4,
+        "browserSupport": "g",
+        "lastUpdated": "2011-06-21 01:24:57"
 }
 
 function detectWeb(doc, url) {
@@ -43,15 +44,15 @@ function doWeb(doc, url) {
 		ids[i] = host+"/full_record.do?" + ids[i];
 	}
 	var product = url.match("product=([^\&]+)\&")[1];
-	for (var i=0 ; i<ids.length; i++) {
-		newDoc = Zotero.Utilities.retrieveDocument(ids[i]);
+	Zotero.Utilities.processDocuments(ids, function(newDoc) { 
 		var url = newDoc.location.href;
 		var sid = newDoc.evaluate('//input[@name="selectedIds"]', newDoc, null, XPathResult.ANY_TYPE, null).iterateNext().value;
 		var nid = newDoc.evaluate('//input[@name="SID"]', newDoc, null, XPathResult.ANY_TYPE, null).iterateNext().value;
 		var post2 = 'product='+product+'&product_sid=' + nid + '&plugin=&product_st_thomas=http://esti.isiknowledge.com:8360/esti/xrpc&export_ref.x=0&export_ref.y=0';
 		var post = 'action=go&mode=quickOutput&product='+product+'&SID=' + nid + '&format=ref&fields=BibAbs&mark_id='+product+'&count_new_items_marked=0&selectedIds=' + sid + '&qo_fields=bib&endnote.x=95&endnote.y=12&save_options=default';
-		Zotero.Utilities.HTTP.retrieveSource('http://apps.isiknowledge.com/OutboundService.do', post);
-		var text = Zotero.Utilities.HTTP.retrieveSource('http://pcs.isiknowledge.com/uml/uml_view.cgi', post2);
+	Zotero.Utilities.doPost('http://apps.isiknowledge.com/OutboundService.do', post,
+        function(t, r) { Zotero.Utilities.doPost('http://pcs.isiknowledge.com/uml/uml_view.cgi',
+                                    post2, function(text, response) {
 		var lines = text.split("\n");
 		var field = " ";
 		var content = " ";
@@ -71,12 +72,12 @@ function doWeb(doc, url) {
 						field = "";
 					}
 				} else if (field == "TI") {
-					item.title = content;
+					item.title = fixCaps(content);
 				} else if (field == "SO") {
 					// People say ISI is bad about being all-caps; let's try this for now
 					// http://forums.zotero.org/discussion/17316
-					if (content.toUpperCase() == content) Zotero.Utilities.capitalizeTitle(content.toLowerCase(), true);
-					item.publicationTitle = content;
+                    // http://forums.zotero.org/discussion/14208
+					item.publicationTitle = fixCaps(content);
 				} else if (field == "SN") {
 					item.ISSN = content;
 				} else if (field == "PD" || field == "PY") {
@@ -112,5 +113,12 @@ function doWeb(doc, url) {
 		}
 		item.attachments = [{url:item.url, title:"ISI Web of Knowledge Snapshot", mimeType:"text/html"}];
 		item.complete();
-	}
+	})});
+    }, function() {Zotero.done()});
+    Zotero.wait();
+}
+
+function fixCaps(text) {
+    if (text.toUpperCase() == text && text.indexOf(" ") !== -1) return Zotero.Utilities.capitalizeTitle(text, true);
+    else return text;
 }
