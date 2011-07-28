0ae805ab1af60650c2d24be34769a60147e9b7ca
Avram Lyon <ajlyon@gmail.com>
2011-07-29 00:44:11 +0400

    More cleanup

--- plugins/zotfile@columbia.edu/chrome/content/zotfile/include.js
+++
@@ -1,7 +0,0 @@
-// Only create main object once
-if (!Zotero.ZotFile) {
-	const zotfileLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-					.getService(Components.interfaces.mozIJSSubScriptLoader);
-	zotfileLoader.loadSubScript("chrome://zotfile/content/zotfile.js");
-	window.addEventListener('load', function(e) { Zotero.ZotFile.init(); }, false);
-}
